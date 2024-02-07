81b7801bc69f13d299ad814cb4dad3760dbc45f0
GennYoon <yoonwonyoul@webchemist.net>
2024-02-07 11:43:01 +0900

    remove output:export

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: { unoptimized: true },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
