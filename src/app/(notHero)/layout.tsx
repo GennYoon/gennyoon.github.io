7ea9b3f2fef51003dcc0139ce63e8f787389438c
GennYoon <yoonwonyoul@webchemist.net>
2024-02-12 21:00:21 +0900

    gloabal head script

--- src/app/(notHero)/layout.tsx
+++ src/app/(notHero)/layout.tsx
@@ -1,64 +1,13 @@
-import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/css/globals.css";
-
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { twMerge } from "tailwind-merge";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { GoogleAnalytics } from "@next/third-parties/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-};
-
-export const metadata: Metadata = {
-  title: "GennYoon Blog",
-  description: "GennYoon Blog",
-  keywords: ["React", "NextJS", "Supabase", "GennYoon", "Blog"],
-  authors: {
-    name: "GennYoon",
-    url: "https://portfolio.gennyoon.net",
-  },
-  openGraph: {
-    type: "website",
-    url: "https://gennyoon.net",
-    title: "GennYoon Blog",
-    images: {
-      url: "https://udakkdpxfzwyalqyjmiz.supabase.co/storage/v1/object/public/images/meta-image.png",
-      width: 1200,
-      height: 630,
-    },
-    description: "GennYoon Blog",
-    siteName: "GennYoon Blog",
-    locale: "ko_KR",
-  },
-  twitter: {
-    card: "summary",
-    site: "@yoonwonyoul",
-    title: "GennYoon Blog",
-    description: "GennYoon Blog",
-    creator: "@yoonwonyoul",
-    images: "https://udakkdpxfzwyalqyjmiz.supabase.co/storage/v1/object/public/images/meta-image.png",
-  },
-};
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={twMerge("min-h-screen bg-background font-sans antialiased", inter.className)}>
-        <GoogleAnalytics gaId="G-51WGHWCJ9G" />
-        <ThemeProvider attribute="class" defaultTheme="dark" storageKey="blog-ui-theme" enableSystem disableTransitionOnChange>
-          <Header />
-          <main className="flex flex-col items-center w-full max-w-[1400px] ml-auto mr-auto pt-28">{children}</main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="dark" storageKey="blog-ui-theme" enableSystem disableTransitionOnChange>
+      <Header />
+      <main className="flex flex-col items-center w-full max-w-[1400px] ml-auto mr-auto pt-28">{children}</main>
+      <Footer />
+    </ThemeProvider>
   );
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "@/assets/css/globals.css";
+
+import { twMerge } from "tailwind-merge";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export const metadata: Metadata = {
+  title: "GennYoon Blog",
+  description: "GennYoon Blog",
+  keywords: ["React", "NextJS", "Supabase", "GennYoon", "Blog"],
+  authors: {
+    name: "GennYoon",
+    url: "https://portfolio.gennyoon.net",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://gennyoon.net",
+    title: "GennYoon Blog",
+    images: {
+      url: "https://udakkdpxfzwyalqyjmiz.supabase.co/storage/v1/object/public/images/meta-image.png",
+      width: 1200,
+      height: 630,
+    },
+    description: "GennYoon Blog",
+    siteName: "GennYoon Blog",
+    locale: "ko_KR",
+  },
+  twitter: {
+    card: "summary",
+    site: "@yoonwonyoul",
+    title: "GennYoon Blog",
+    description: "GennYoon Blog",
+    creator: "@yoonwonyoul",
+    images: "https://udakkdpxfzwyalqyjmiz.supabase.co/storage/v1/object/public/images/meta-image.png",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-51WGHWCJ9G`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-51WGHWCJ9G');
+          `,
+          }}
+        />
+      </Head>
+      <body className={twMerge("min-h-screen bg-background font-sans antialiased", inter.className)}>
+        <GoogleAnalytics gaId="G-51WGHWCJ9G" />
+
+        {children}
+      </body>
+    </html>
+  );
+}
